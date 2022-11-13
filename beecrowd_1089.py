d51ed9d1b7fe7e7f61850179efdd688ed78e5dd5
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2022-11-13 20:38:45 -0300

    Loop Musical

---
+++ beecrowd_1089.py
@@ -0,0 +1,19 @@
+
+
+def main():
+	t = int(input())
+
+	while (t > 0):
+
+		n = int(input())
+
+		v =  [int (x) for x in input().split(" ")]
+
+		print(v)
+
+
+		result = []
+		print(len(result) -1)
+		t -=1 
+	
+main()
