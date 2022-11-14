b14952021fdaaccfe5381084aa11360dc6b0c3f2
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2022-11-13 23:01:33 -0300

    Loop Musical

--- beecrowd_1089.py
+++ beecrowd_1089.py
@@ -1,19 +1,35 @@
+#Loop Musical
 
+def get_picos(wave, result):
+
+  for i in range(len(wave)):
+    if i == 0 :
+      now, prev, nex = wave[i], wave[-1], wave[i +1]
+    elif i == (len(wave) -1 ) :
+      now, prev, nex =  wave[i], wave[i -1], wave[0]
+    else:
+      now, prev, nex = wave[i], wave[i -1], wave[i + 1]
+
+    delta_i = now - prev 
+    delta_j = nex - now
+
+    if not ( ( delta_i * delta_j ) > 0 ):
+      result.append(1)
 
 def main():
-	t = int(input())
 
-	while (t > 0):
+  while (True):
 
-		n = int(input())
+    n = int(input())
 
-		v =  [int (x) for x in input().split(" ")]
+    if n == 0:
+      break
 
-		print(v)
+    wave = [int (x) for x in input().split(" ")]
 
+    result = []
+    get_picos(wave, result)
 
-		result = []
-		print(len(result) -1)
-		t -=1 
-	
+    print(len(result))
+      
 main()
