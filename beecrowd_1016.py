ee7ad29320d48612ac980da9ca78e90ca8a2ee5c
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2022-12-02 20:54:00 -0300

    distância

---
+++ beecrowd_1016.py
@@ -0,0 +1,3 @@
+x = int(input())
+
+print(str(2 * x) + " minutos")
---
+++ beecrowd_1144.py
@@ -0,0 +1,8 @@
+
+n = int(input())
+
+for i in range(1, n + 1):
+  a = (i**2)
+  b = (i**3)
+  print(str(i) + " " + str(a) + " " + str(b))
+  print(str(i) + " " + str(a + 1) + " " + str(b + 1))
