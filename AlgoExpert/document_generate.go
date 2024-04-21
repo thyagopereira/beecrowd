68bd010966270539c1ac4db2213092c836707fd0
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2024-04-21 17:16:12 -0300

    document Generate O(n) time | o(n) space

---
+++ AlgoExpert/document_generate.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func GenerateDocument(characters string, document string) bool {
+	freqChar := make(map[string]int)
+	for _, r := range characters {
+		freqChar[string(r)] += 1
+	}
+
+	freqDocument := make(map[string]int)
+	for _, r := range document {
+		freqDocument[string(r)] += 1
+	}
+
+	for _, k := range document {
+		if _, ok := freqChar[string(k)]; !ok {
+			return false
+		} else if freqChar[string(k)] < freqDocument[string(k)] {
+			return false
+		}
+	}
+
+	return true
+
+}
+
+func main() {
+	characters := "Bste!hetsi ogEAxpelrt x "
+	document := "AlgoExpert is the Best!"
+
+	expected := true
+	output := GenerateDocument(characters, document)
+
+	fmt.Println(output)
+
+	if output != expected {
+		panic("Wrong answer")
+	}
+}
