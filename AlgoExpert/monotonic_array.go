8275e006ed088c0f8a6b977d6a151aa95a798075
thyagopereira <thyago.pereira.silva@ccc.ufcg.edu.br>
2024-05-11 13:48:54 -0300

    Monotonic_array O(n) time O(1) space

---
+++ AlgoExpert/monotonic_array.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func IsMonotonic(array []int) bool {
+
+	if len(array) < 2 {
+		return true
+	}
+
+	p, n := 0, 1
+	decreasing, increasing := false, false
+
+	for n < len(array) {
+		if array[n] < parray[p] {
+			decreasing = true
+		} else if array[n] > array[p] {
+			increasing = true
+		}
+		p++
+		n++
+	}
+
+	return !(decreasing && increasing)
+
+}
+
+func main() {
+	input := []int{1, 5, 10}
+	output := IsMonotonic(input)
+
+	fmt.Println(output)
+	expected := true
+	if expected != output {
+		panic("Wrong answer")
+	}
+
+}
